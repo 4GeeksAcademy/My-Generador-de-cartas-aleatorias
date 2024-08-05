631e5178c72fcd11c9332977755ffcf9641bbfca
octavio ruben flores ramirez <ruben94fr@hotmail.com>
2024-08-05 17:07:09 +0000

    entrega

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let palo = ["♦", "♥", "♠", "♣"];
+  let valor = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let color = "";
+
+  let palorandom = Math.floor(Math.random() * palo.length);
+  let valorrandom = Math.floor(Math.random() * valor.length);
+  if (palo[palorandom] === "♦" || palo[palorandom] === "♥") {
+    color = "red";
+  } else {
+    color = "black";
+  }
+  let valorpalohead = document.getElementById("palocab");
+
+  valorpalohead.textContent = palo[palorandom];
+  valorpalohead.style.color = color;
+  let valornumero = document.getElementById("numero");
+  valornumero.innerHTML = valor[valorrandom];
+  let valorpalofoot = document.getElementById("palopie");
+  valorpalofoot.textContent = palo[palorandom];
+  valorpalofoot.style.color = color;
 };
