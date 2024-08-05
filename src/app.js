aa0f1358000ad9a98b691be21108a1031778d4f0
octavio ruben flores ramirez <ruben94fr@hotmail.com>
2024-08-05 21:27:57 +0000

    separar funciones

--- src/app.js
+++ src/app.js
@@ -2,9 +2,28 @@ let palo = ["♦", "♥", "♠", "♣"];
 let valor = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 let color = "";
 
-window.onload = function() {
-  //write your code here
+window.onload = generarRandom();
 
+let contador = 11;
+
+let boton = document.getElementById("btn-random");
+boton.addEventListener("click", generarRandom);
+boton.addEventListener("click", resetContador);
+
+let secundero = setInterval(function() {
+  contador--;
+  document.getElementById("reloj").textContent = contador;
+  if (contador === 0) {
+    contador = 11;
+    generarRandom();
+  }
+}, 1000);
+
+function resetContador() {
+  contador = 11;
+}
+
+function generarRandom() {
   let palorandom = Math.floor(Math.random() * palo.length);
   let valorrandom = Math.floor(Math.random() * valor.length);
   if (palo[palorandom] === "♦" || palo[palorandom] === "♥") {
@@ -21,23 +40,4 @@ window.onload = function() {
   let valorpalofoot = document.getElementById("palopie");
   valorpalofoot.textContent = palo[palorandom];
   valorpalofoot.style.color = color;
-};
-
-let contador = 11;
-
-let boton = document.getElementById("btn-random");
-boton.addEventListener("click", window.onload);
-boton.addEventListener("click", resetContador);
-
-let secundero = setInterval(function() {
-  contador--;
-  document.getElementById("reloj").textContent = contador;
-  if (contador === 0) {
-    contador = 11;
-    window.onload();
-  }
-}, 1000); // Actualiza cada segundo (1000 ms)
-
-function resetContador() {
-  contador = 11;
 }
